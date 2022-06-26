963ecd90b310dd5891e05b44648b0ff46deeea86
moamen2020 <m01144944981@gmail.com>
2022-06-27 00:50:01 +0200

    feature/theming-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import'bootstrap/dist/css/bootstrap.min.css';
-import'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import "./assets/scss/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
