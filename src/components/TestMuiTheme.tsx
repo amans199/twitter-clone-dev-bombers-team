25a973a9b0cba4e0288f20a2d9eabe14d9e523d0
geodeveloper95 <mohamed.abdelnaser159@gmail.com>
2022-07-01 04:38:31 +0200

    edite some bugs and merge same files and delete the same files to run the project

--- src/components/TestMuiTheme.tsx
+++ src/components/TestMuiTheme.tsx
@@ -1,19 +1,21 @@
 import { Button, Box, Typography } from '@mui/material';
-
+import AppleIcon from '@material-ui/icons/Apple';
+import GenericButton from '../components/GenericButton';
 
 const TestMuiTheme = () => {
     return (
         <div>
             <Box height='300px' bgcolor='dimMode.main' width= '300px' margin='30px auto' padding='20px'>
                 <Typography color='#fff' 
-                    sx={{display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', 
+                        marginBottom: '20px' }}>
                     This is a warning box
                 </Typography>
-                <Button variant='contained' color='error' 
-                    sx={{fontSize: '14px', margin:'20px auto', color: '#fff'}} >
+                <Button variant='contained' color='primary' 
+                    sx={{fontSize: '14px', marginBottom: '30px !important', color: '#fff'}} >
                     Click here
                 </Button>
-
+                <GenericButton label="Sign up with Apple" icon={<AppleIcon />} />
             </Box>
         </div>
     )
